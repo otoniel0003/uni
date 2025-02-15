59ef6f441f93e49c1a49de2e91695b8f78a08e3a
otoniel0003 <l24550281@chihuahua2.tecnm.mx>
2025-02-15 14:25:13 -0600

    Add files via upload

---
+++ Modificadores_Eva1/src/main/java/OtrasClases/OtrasClases.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package OtrasClases;
+
+/**
+ *
+ * @author Salid
+ */
+public class OtrasClases {
+public class OtraClase {
+    public int valorPublico;
+    private int valorPrivado;
+    int valorDefault;
+}
+
+class Prueba{    
+}
+}
---
+++ Modificadores_Eva1/src/main/java/com/mycompany/modificadores_eva1/Modificadores_Eva1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.modificadores_eva1;
+import OtrasClases.OtraClases;
+/**
+ *
+ * @author Salid
+ */
+public class Modificadores_Eva1 {
+
+    public static void main(String[] args) {
+  
+            // TODO code application logic here
+        OtrasClase otraClase = new OtrasClase();
+        int valorPublico = otraClase.valorPublico;
+        //Prueba Elote = new Prueba; f
+        //Prueba mo es visible, su modificador es 
+        //default, solo se puede utilizar
+        //el paquete 
+}
+}   
